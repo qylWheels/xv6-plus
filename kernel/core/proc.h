aae6085cd2b8b3863e9d2dc22362884dc38b4fbd
qylWheels <command1748165360@126.com>
2026-03-15 00:11:41 +0800

    实现对缺页异常的trace

--- kernel/core/proc.h
+++ kernel/core/proc.h
@@ -109,6 +109,7 @@ struct proc
   int killed;           // If non-zero, have been killed
   int xstate;           // Exit status to be returned to parent's wait
   int pid;              // Process ID
+  uint pgfaults;        // 缺页异常发生次数
 
   // wait_lock must be held when using this:
   struct proc *parent; // Parent process
