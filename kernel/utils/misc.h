cf6ae22085e71375f5ce4033ae2043812d0d5ade
qylWheels <command1748165360@126.com>
2026-03-30 19:59:10 +0800

    为null的定义添加guard

--- kernel/utils/misc.h
+++ kernel/utils/misc.h
@@ -1,8 +1,10 @@
 #ifndef _UTILS_MISC_H_
 #define _UTILS_MISC_H_
 
+#ifndef NULL
 #define NULL 0
+#endif  // NULL
 
 #define NELEM(x) (sizeof(x) / sizeof((x)[0]))
 
-#endif // _UTILS_MISC_H_
+#endif  // _UTILS_MISC_H_
