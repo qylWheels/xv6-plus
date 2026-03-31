6b1eb847f3e0e8953471fc8b59f3c1713cf7ecf7
qylWheels <command1748165360@126.com>
2026-03-31 21:39:08 +0800

    对外暴露kmallocinit接口

--- kernel/mm/kmalloc.h
+++ kernel/mm/kmalloc.h
@@ -2,6 +2,9 @@
 
 #include <core/types.h>
 
+// 初始化kmalloc模块
+void kmallocinit(void);
+
 // 参数size：size=0时返回NULL；size>0且size<=2048时分配size字节内存；size>2048时返回NULL
 // 返回值：若size=0，返回NULL；若size>0，成功时返回指向已分配内存的指针，
 // 失败时panic
