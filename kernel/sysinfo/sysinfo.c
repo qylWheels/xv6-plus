4697fef662da999e632467f9d84d1f58f6b0f317
qylWheels <command1748165360@126.com>
2026-04-16 00:08:10 +0800

    修复sysinfo没有使用copyout将数据拷贝到用户空间的问题

--- kernel/sysinfo/sysinfo.c
+++ kernel/sysinfo/sysinfo.c
@@ -1,21 +1,49 @@
 #include <core/proc.h>
 #include <mm/kalloc.h>
 #include <mm/kmalloc.h>
+#include <mm/vm.h>
 #include <sysinfo/sysinfo.h>
 #include <utils/string.h>
 
 int ksysinfo(const char* path, struct sysinfo* p) {
   if (0 == strcmp(path, "/memory/phys")) {
     extern struct kmem kmem;
-    p->u.memory.phys.total_pgs = kmem.total;
-    p->u.memory.phys.used_pgs = kmem.total - kmem.free;
-    p->u.memory.phys.free_pgs = kmem.free;
-    p->u.memory.phys.alloc_times = kmem.alloc_times;
-    p->u.memory.phys.free_times = kmem.free_times;
-    return 0;
-  } else if (0 == strcmp(path, "memory/virt/self")) {
+    extern int kalloc_times;
+    extern struct spinlock kalloc_cnt_lock;
+    extern int kfree_times;
+    extern struct spinlock kfree_cnt_lock;
+
+    acquire(&kmem.lock);
+
     struct proc* proc = myproc();
-    p->u.memory.virt.self.pgfault_cnt = proc->pgfaults;
+
+    copyout(proc->pagetable, (uint64)&p->u.memory.phys.total_pgs,
+            (char*)&kmem.total, sizeof(kmem.total));
+
+    uint64 used_pgs = kmem.total - kmem.free;
+    copyout(proc->pagetable, (uint64)&p->u.memory.phys.used_pgs,
+            (char*)&used_pgs, sizeof(used_pgs));
+
+    copyout(proc->pagetable, (uint64)&p->u.memory.phys.free_pgs,
+            (char*)&kmem.free, sizeof(kmem.free));
+
+    acquire(&kalloc_cnt_lock);
+    acquire(&kfree_cnt_lock);
+    copyout(proc->pagetable, (uint64)&p->u.memory.phys.alloc_times,
+            (char*)&kalloc_times, sizeof(kalloc_times));
+    copyout(proc->pagetable, (uint64)&p->u.memory.phys.free_times,
+            (char*)&kfree_times, sizeof(kfree_times));
+    release(&kfree_cnt_lock);
+    release(&kalloc_cnt_lock);
+
+    release(&kmem.lock);
+    return 0;
+  } else if (0 == strcmp(path, "/memory/virt/self")) {
+    struct proc* proc = myproc();
+    acquire(&proc->lock);
+    copyout(proc->pagetable, (uint64)&p->u.memory.virt.self.pgfault_cnt,
+            (char*)&proc->pgfaults, sizeof(proc->pgfaults));
+    release(&proc->lock);
     return 0;
   } else {
     return -EPATH;
