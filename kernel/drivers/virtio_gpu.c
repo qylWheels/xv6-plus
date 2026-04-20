23017f255a97cd515c3d67ac512faae88b12d890
qylWheels <command1748165360@126.com>
2026-04-20 21:01:44 +0800

    1、新增desc分配和释放机制 2、封装部分字段进入gpu结构体

--- kernel/drivers/virtio_gpu.c
+++ kernel/drivers/virtio_gpu.c
@@ -108,21 +108,66 @@ struct virtio_gpu_resp_display_info {
   } pmodes[VIRTIO_GPU_MAX_SCANOUTS];
 };
 
+struct queue {
+  char free_desc[MAX_QUEUE_SIZE];   // 1为空闲，0为正在被使用
+  struct virtq_desc* desc_ring;     // desc[]
+  struct virtq_avail* driver_ring;  // 只需一个，其中已经包含整个环形队列了
+  struct virtq_used* device_ring;   // 依旧只需一个，其中包含整个环形队列
+  uint32 last_used_idx;             // 最后一个处理完毕的device_ring的下标
+};
+
 struct gpu {
-  struct {
-    struct virtq_desc* desc_ring;
-    struct virtq_avail* driver_ring;
-    struct virtq_used* device_ring;
-  } controlq;
-  struct {
-    struct virtq_desc* desc_ring;
-    struct virtq_avail* driver_ring;
-    struct virtq_used* device_ring;
-  } cursorq;
-  uint32 last_used_idx;
+  struct queue controlq;
+  struct queue cursorq;
+  // TODO: 增加一个锁供os正常工作时使用
 } gpu;
 
+// -1代表desc[]中的desc都正在被占用
+static int alloc_desc(struct queue* q) {
+  for (int i = 0; i < MAX_QUEUE_SIZE; ++i) {
+    if (q->free_desc[i]) {
+      q->free_desc[i] = 0;  // 设为被占用
+      return i;
+    }
+  }
+  return -1;
+}
+
+static void free_desc(struct queue* q, int idx) {
+  q->desc_ring[idx].addr = 0;
+  q->desc_ring[idx].len = 0;
+  q->desc_ring[idx].flags = 0;
+  q->desc_ring[idx].next = 0;
+  q->free_desc[idx] = 1;  // 设为空闲
+}
+
+static void free_desc_chain(struct queue* q, int idx) {
+  while (1) {
+    int flag = q->desc_ring[idx].flags;
+    int next = q->desc_ring[idx].next;
+    free_desc(q, idx);
+    if (flag & VRING_DESC_F_NEXT) {
+      idx = next;
+    } else {
+      break;
+    }
+  }
+}
+
+// 初始化gpu结构体
+static void init_gpu_struct(void) {
+  gpu.controlq.last_used_idx = 0;
+  gpu.cursorq.last_used_idx = 0;
+  for (int i = 0; i < MAX_QUEUE_SIZE; ++i) {
+    gpu.controlq.free_desc[i] = 1;
+    gpu.cursorq.free_desc[i] = 1;
+  }
+}
+
 void drivers_virtio_gpu_init(void) {
+  // 初始化gpu结构体先
+  init_gpu_struct();
+
   // 检查magic和version
   uint32 magic = MMIO(VIRTIO_MMIO_MAGIC_VALUE);
   uint32 version = MMIO(VIRTIO_MMIO_VERSION);
@@ -212,28 +257,32 @@ void drivers_virtio_gpu_init(void) {
   MMIO(VIRTIO_MMIO_STATUS) |= VIRTIO_CONFIG_S_DRIVER_OK;
 
   // 获取virtio-gpu显示信息
-  uint16 head_idx = 0;  // 第一个空闲描述符索引
+  int head_idx = alloc_desc(&gpu.controlq);    // 第一个空闲描述符索引
+  int second_idx = alloc_desc(&gpu.controlq);  // 第二个索引
+  printf("head=%d,second=%d\n", head_idx, second_idx);
   struct virtio_gpu_ctrl_hdr cmd_hdr = {0};
   cmd_hdr.type = VIRTIO_GPU_CMD_GET_DISPLAY_INFO;
   cmd_hdr.flags = VIRTIO_GPU_FLAG_FENCE;
   gpu.controlq.desc_ring[head_idx].addr = (uint64)&cmd_hdr;
   gpu.controlq.desc_ring[head_idx].len = sizeof(cmd_hdr);
   gpu.controlq.desc_ring[head_idx].flags =
-      0 | VRING_DESC_F_NEXT;                             // 0 = 设备只读
-  gpu.controlq.desc_ring[head_idx].next = head_idx + 1;  // 指向下一个描述符
+      0 | VRING_DESC_F_NEXT;                           // 0 = 设备只读
+  gpu.controlq.desc_ring[head_idx].next = second_idx;  // 指向下一个描述符
 
   struct virtio_gpu_resp_display_info resp_buf = {0};
-  gpu.controlq.desc_ring[head_idx + 1].addr = (uint64)&resp_buf;
-  gpu.controlq.desc_ring[head_idx + 1].len = sizeof(resp_buf);
-  gpu.controlq.desc_ring[head_idx + 1].flags = VRING_DESC_F_WRITE;  // 设备可写
-  gpu.controlq.desc_ring[head_idx + 1].next = 0;                    // 链结束
+  gpu.controlq.desc_ring[second_idx].addr = (uint64)&resp_buf;
+  gpu.controlq.desc_ring[second_idx].len = sizeof(resp_buf);
+  gpu.controlq.desc_ring[second_idx].flags = VRING_DESC_F_WRITE;  // 设备可写
+  gpu.controlq.desc_ring[second_idx].next = 0;                    // 链结束
 
   uint16 avail_idx = gpu.controlq.driver_ring->idx;
-  gpu.controlq.driver_ring->ring[avail_idx] = head_idx;  // 放入描述符链头索引
+  gpu.controlq.driver_ring->ring[avail_idx % NUM] =
+      head_idx;  // 放入描述符链头索引
 
   __sync_synchronize();  // 确保 ring 写入完成再更新 idx【spec 2.7.13.3.1】
-  gpu.controlq.driver_ring->idx++;  // 原子递增，使描述符对设备可见
-  uint32 last_used_idx = gpu.controlq.device_ring->idx;
+  // 原子递增，使描述符对设备可见，不需要对NUM取余，因为取下标的时候会取余
+  gpu.controlq.driver_ring->idx++;
+  gpu.controlq.last_used_idx = gpu.controlq.device_ring->idx;
   __sync_synchronize();  // 确保 idx 更新对设备可见【spec 2.7.13.4.1】
   MMIO(VIRTIO_MMIO_QUEUE_NOTIFY) = 0;
 
@@ -244,16 +293,15 @@ void drivers_virtio_gpu_init(void) {
     // printf("device->used=%d, last_used=%d\n",
     //        QUEUE(used, device_area_controlq)->idx, last_used_idx);
     __sync_synchronize();
-    if (gpu.controlq.device_ring->idx != last_used_idx) {
+    if (gpu.controlq.device_ring->idx != gpu.controlq.last_used_idx) {
       // 内存屏障：确保读取到最新的 used ring 内容【隐含在规范要求中】
       __sync_synchronize();
-      uint16 used_pos = last_used_idx % MAX_QUEUE_SIZE;
-      struct virtq_used_elem* e =
-          &gpu.controlq.device_ring->ring[used_pos];
+      uint16 used_pos = gpu.controlq.last_used_idx % MAX_QUEUE_SIZE;
+      struct virtq_used_elem* e = &gpu.controlq.device_ring->ring[used_pos];
 
       if (e->id == head_idx &&
           resp_buf.hdr.type == VIRTIO_GPU_RESP_OK_DISPLAY_INFO) {
-        printf("resp_buf.hdr.type=0x%x\n", resp_buf.hdr.type);
+        // printf("resp_buf.hdr.type=0x%x\n", resp_buf.hdr.type);
         // 此时 resp_buf 已包含有效的显示信息
         for (int i = 0; i < VIRTIO_GPU_MAX_SCANOUTS; i++) {
           uint32 ena = resp_buf.pmodes[i].enabled;
@@ -266,9 +314,11 @@ void drivers_virtio_gpu_init(void) {
         }
       }
 
-      // 更新驱动本地指针，并释放描述符 5 和 6 以供下次使用
-      last_used_idx++;
-      // free_descriptors(head_idx, 2);
+      // 更新驱动本地指针，不用取余
+      gpu.controlq.last_used_idx++;
+
+      // 处理完毕，释放descs
+      free_desc_chain(&gpu.controlq, head_idx);
 
       break;
     }
